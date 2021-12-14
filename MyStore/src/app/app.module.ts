0f41903815e6a9266a1c93c758f4750fedabc0ce
warp9training <uncommondev344@gmail.com>
2021-12-14 23:00:51 +0000

    Initial Push
    Roughly scaffolded what is required for the project.
    
    This is the initial push of the created files

---
+++ MyStore/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ProductlistComponent,
+    CartComponent,
+    OrderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
