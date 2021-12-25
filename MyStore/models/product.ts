a97937569f5ef52e397a0250000e5327e95bdecf
warp9training <uncommondev344@gmail.com>
2021-12-25 15:42:07 +0000

    Updated Product Model
    Initialised the quantity property

--- MyStore/models/product.ts
+++ MyStore/models/product.ts
@@ -12,5 +12,6 @@ export default class Product {
         this.price = 1.99,
         this.url = "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
         this.description = "Really nice product!"
+        this.quantity = 1
     }
 }
