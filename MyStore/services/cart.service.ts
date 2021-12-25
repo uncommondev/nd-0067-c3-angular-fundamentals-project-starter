336bd86a841c1125a8efba3c8c17209696f0640d
warp9training <uncommondev344@gmail.com>
2021-12-25 15:43:09 +0000

    Updated The Cart
    Added all requirements needed for the cart process

--- MyStore/services/cart.service.ts
+++ MyStore/services/cart.service.ts
@@ -12,12 +12,40 @@ export class CartService {
     this.cart = []
   }
 
-  addToCart(product: Product) {
-    this.cart.push(product)
+  getCart(){
+    console.log(`getCart`)
+    console.log(this.cart)
+    return this.cart
   }
 
-  deleteFromCart(product: Product){
-    const newCart = this.cart.filter(item => item.id !== product.id)
+  addToCart(product: Product) {
+    this.cart.push(product)
+    console.log(`addToCart`)
+    console.log(this.cart)
+  }
+
+  deleteFromCart(id: number){
+    const newCart = this.cart.filter(item => item.id !== id)
     this.cart = newCart
+    console.log(`deleteFromCart`)
+    console.log(this.cart)
+  }
+
+  updateQuantity(quantity: number, index: number){
+    this.cart[index].quantity = quantity
+    console.log(`updateQuantity`)
+    console.log(this.cart)
+  }
+
+  cartTotal(): string {
+    let total: number = 0
+    this.cart.map(item => {
+      total += item.price * (item.quantity || 1)
+      console.log(`total`)
+      console.log(total)
+    })
+    console.log(`Completed total`)
+    console.log(total.toFixed(2))
+    return(total.toFixed(2))
   }
 }
--- MyStore/src/app/cart/cart-checkout/cart-checkout.component.ts
+++ MyStore/src/app/cart/cart-checkout/cart-checkout.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { CartService } from './../../../../services/cart.service';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import Product from 'models/product';
 
 @Component({
   selector: 'app-cart-checkout',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-checkout.component.css']
 })
 export class CartCheckoutComponent implements OnInit {
+  cartItems: Product[]
+  // Form Elements
+  fullname: string = ""
+  address: string = ""
+  creditcard: number
+  // Cart items
+  cartTotal: string = this.cartService.cartTotal()
 
-  constructor() { }
+  @Output() createOrder: EventEmitter<any> = new EventEmitter()
+
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartItems = this.cartService.getCart()
+    console.log(this.cartItems)
+  }
+
+  checkoutOrder() {
+    // BLOC for order - if required
+    // Proceed to pass the details to the next page -->
+    // Add the event emitter
+    const order = {
+      fullname: this.fullname,
+      address: this.address,
+      creditcard: this.creditcard
+    }
+
+    this.createOrder.emit(order)
+
+    this.fullname = ""
+    this.address = ""
+    this.creditcard
+  }
+
+  updateQuantity(index: number, quant: string ){// ev: Event){ //quantity: number){
+    console.log(`Update Quantity Triggered`)
+    console.log(`Quantity`)
+    console.log(quant)
+    console.log(`Index`)
+    console.log(index)
+    this.cartService.updateQuantity(+quant, index)
+  }
+
+  removeProduct(id: number){
+    const newCart = this.cartItems.filter(item => item.id !== id)
+    this.cartItems = newCart
+    console.log(`deleteFromCart`)
+    console.log(this.cartItems)
+    this.cartService.deleteFromCart(id)
   }
 
 }
